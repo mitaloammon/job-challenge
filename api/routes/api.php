68c65b2735dde751baff3f0f703ee1e7e3b75070
Mitalo Ammon <92004593+mitaloammon@users.noreply.github.com>
2024-11-13 17:11:39 -0300

    update routes/api

--- api/routes/api.php
+++ api/routes/api.php
@@ -7,11 +7,11 @@ use App\Http\Middleware\JwtMiddleware;
 
 Route::apiResource('posts', PostController::class);
 
-Route::post('/register', [JWTAuthController::class, 'register']);
-Route::post('/login', [JWTAuthController::class, 'login']);
+Route::post('register', [JWTAuthController::class, 'register']);
+Route::post('login', [JWTAuthController::class, 'login']);
 
-Route::post('/logout', [JWTAuthController::class, 'logout'])->middleware('jwt.auth');
+Route::post('logout', [JWTAuthController::class, 'logout'])->middleware('jwt.auth');
 
 Route::middleware([JwtMiddleware::class])->group(function () {
-    Route::get('/user', [JWTAuthController::class, 'getUser']);
+    Route::get('user', [JWTAuthController::class, 'getUser']);
 });
