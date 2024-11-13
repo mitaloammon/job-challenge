c96713bcedf6888febf1f7660131f7f1a6286d6a
Mitalo Ammon <92004593+mitaloammon@users.noreply.github.com>
2024-11-13 19:54:10 -0300

    update file app

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -8,11 +8,12 @@ use App\Http\Middleware\JwtMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(JwtMiddleware::class);
+        //$middleware->append(JwtMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
